f68359f2dee08f8245e6576b921111624e11f04f
unknown <smith.dev@gmail.com>
2023-03-04 23:55:29 +0800

    saving: progress

---
+++ src/app/zoo/zoo/zoo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ZooComponent } from './zoo.component';
+
+
+
+@NgModule({
+  declarations: [
+    ZooComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ZooModule { }
